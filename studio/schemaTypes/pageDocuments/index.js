80732b155fb91db6dbfa358e013ad5165cb40e0e
Coco K <coco@mac.mynetworksettings.com>
2025-09-19 12:04:11 -0400

    removed managed fund page

--- studio/schemaTypes/pageDocuments/index.js
+++ studio/schemaTypes/pageDocuments/index.js
@@ -3,14 +3,12 @@ import childCare from './childCare.js';
 import ourProcess from './ourProcess.js';
 import whatWeLookFor from './whatWeLookFor.js';
 import whoWeAre from './whoWeAre.js';
-import managedFund from './managedFund.js';
 import investmentStrategy from './investmentStrategy.js'
 
 export default [
     home, 
 
     whatWeLookFor,
-    managedFund,
     investmentStrategy, 
     whoWeAre, 
     
